d646bbaffb6aad4a5fd3cd2b867136f304cf2909
Sorvani <sorvani@gmail.com>
2018-03-21 15:27:19 -0500

    updated to include new files
    Added .htaccess and .user.ini to the files with the required context.

--- Nextcloud/selinux_config.sh
+++ Nextcloud/selinux_config.sh
@@ -5,12 +5,11 @@ httpdrw='httpd_sys_rw_content_t'
 setsebool -P httpd_can_sendmail 1
 setsebool -P httpd_can_network_connect 1
 
-printf "Set SELinux HTTPD RW on  Directories\n"
+printf "Set SELinux HTTPD RW on directories and files\n"
 semanage fcontext -a -t ${httpdrw} "${ocpath}/config(/.*)?"
-restorecon -R ${ocpath}/config
 semanage fcontext -a -t ${httpdrw} "${ocpath}/apps(/.*)?"
-restorecon -R ${ocpath}/apps
 semanage fcontext -a -t ${httpdrw} "${ocpath}/data(/.*)?"
-restorecon -R ${ocpath}/data
 semanage fcontext -a -t ${httpdrw} "${ocpath}/updater(/.*)?"
-restorecon -R ${ocpath}/updater
+semanage fcontext -a -t ${httpdrw} '${ocpath}/.htaccess'
+semanage fcontext -a -t ${httpdrw} '${ocpath}/.user.ini'
+restorecon -Rv ${ocpath}
