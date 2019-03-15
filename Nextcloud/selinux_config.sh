027f048c0117ff67cda9aeb3269177dd423ecbfe
Sorvani <sorvani@gmail.com>
2019-03-15 16:46:52 -0500

    give entire fodler HTTPD_RW

--- Nextcloud/selinux_config.sh
+++ Nextcloud/selinux_config.sh
@@ -5,11 +5,6 @@ httpdrw='httpd_sys_rw_content_t'
 setsebool -P httpd_can_sendmail 1
 setsebool -P httpd_can_network_connect 1
 
-printf "Set SELinux HTTPD RW on directories and files\n"
-semanage fcontext -a -t ${httpdrw} "${ocpath}/config(/.*)?"
-semanage fcontext -a -t ${httpdrw} "${ocpath}/apps(/.*)?"
-semanage fcontext -a -t ${httpdrw} "${ocpath}/data(/.*)?"
-semanage fcontext -a -t ${httpdrw} "${ocpath}/updater(/.*)?"
-semanage fcontext -a -t ${httpdrw} '${ocpath}/.htaccess'
-semanage fcontext -a -t ${httpdrw} '${ocpath}/.user.ini'
+printf "Set SELinux HTTPD RW on entirenextcloud folder\n"
+semanage fcontext -a -t ${httpdrw} "${ocpath}(/.*)?"
 restorecon -FRv ${ocpath}
